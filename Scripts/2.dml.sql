095ce121582cefdb6ef74251841ae3ef600dfff9
jylee <stpn94@gmail.com>
2021-05-11 09:16:59 +0900

    mybatis study 초기값 설정

---
+++ Scripts/2.dml.sql
@@ -0,0 +1,5 @@
+DROP USER IF EXISTS 'user_mybatis_study'@'localhost';
+
+GRANT ALL PRIVILEGES
+	ON mybatis_study.*
+	TO 'user_mybatis_study'@'localhost' identified BY 'rootroot';
---
+++ Scripts/4.test.sql
@@ -0,0 +1,5 @@
+SELECT * FROM addresses;
+SELECT * FROM course_enrollment;
+SELECT * FROM courses;
+SELECT * FROM students;
+SELECT * FROM tutors;
